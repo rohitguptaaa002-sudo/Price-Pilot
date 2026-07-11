5d3e4c9378ace1b3f35f1901481e1fbf0c70aba3
Gaganprit singh <gaganpritsingh@Gaganprits-MacBook-Pro.local>
2026-07-11 18:19:04 +0530

    Fix waitUntil typo

--- backend/Services/checkStock.js
+++ backend/Services/checkStock.js
@@ -29,7 +29,7 @@ const checkStock = async (product) => {
     page.setDefaultNavigationTimeout(30000);
 
     await page.goto(product.url, {
-      waitUntil: "commmit",
+      waitUntil: "domcontentloaded",
       timeout: 15000,
     });
 
